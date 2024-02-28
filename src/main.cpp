88709c870afd6438ba6199f284036e34807b758e
warriormaster12 <streng.alexander@outlook.com>
2024-02-28 12:49:30 +0200

    Improve InputText

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,25 @@ int main() {
   moves = position.generate_legal_moves();
   position.render_legal_moves(moves);
   position.render_board();
+  char coords[5] = "";
   while (renderer.is_active() && moves.size() > 0) {
     FrameInfo info = renderer.prepare_frame(position);
     ImGui::SetNextWindowSizeConstraints(ImVec2(300, 300), ImVec2(640, 480));
     ImGui::Begin("Chess");
     ImGui::Text(position.get_moving_player() == WHITE ? "White's turn" : "Black's turn");
-    char coords[5] = "";
-    ImGui::InputText("where to move", coords, 5);
+    ImGui::SetKeyboardFocusHere(); // keeps the cursor in focus after hitting enter
+    ImGui::InputText(
+      "where to move", 
+      coords, 
+      5, 
+      ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EscapeClearsAll | ImGuiInputTextFlags_AlwaysOverwrite
+    );
     size_t char_count = strlen(coords);
     Move player_move = Move({-1,-1}, {-1, -1});
     if (char_count == 2) {
       
     }
-    if (char_count == 4) {
+    if (char_count == 4 && ImGui::IsKeyPressed(ImGuiKey_Enter)) {
       bool valid_coords = false;
       for (Move& move : moves) {
         if (move.get_coords() == coords) {
